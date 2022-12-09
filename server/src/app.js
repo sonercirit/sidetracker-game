8b63b4edd413500c469b5545e4b6f77df16ce4c4
sonercirit <sonercirit@gmail.com>
2022-12-09 06:41:12 +0300

    move handling moved to websocket

--- server/src/app.js
+++ server/src/app.js
@@ -1,24 +1,45 @@
 import express from "express";
 import * as dotenv from "dotenv";
+import { WebSocketServer } from "ws";
 import player from "./player/index.js";
 import game from "./game/index.js";
-import move from "./move/index.js";
+import handleMove from "./move/index.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
+const wss = new WebSocketServer({ port: parseInt(port, 10) + 1 });
 
-app.get("/", (req, res) => {
-  res.send("side-stacker game server");
+const mapped = {};
+wss.on("connection", function connection(ws) {
+  ws.on("message", async function message(data) {
+    const parsed = JSON.parse(data);
+
+    // register the current user to the mapping
+    if (!mapped[parsed.gameId]) {
+      mapped[parsed.gameId] = {};
+    }
+    mapped[parsed.gameId][parsed.playerId] = ws;
+
+    // finish early if type is register
+    if (parsed.type === "register") {
+      return;
+    }
+
+    const response = await handleMove(parsed);
+    Object.values(mapped[parsed.gameId]).forEach((socket) => {
+      socket.send(JSON.stringify(response));
+    });
+  });
 });
+
+app.use(express.json());
 
 app.use("/players", player);
 app.use("/games", game);
-app.use("/moves", move);
 
 app.listen(port, () => {
-  console.log(`SSGM app listening on port ${port}`);
+  console.log(`STGM app listening on port ${port}`);
 });
--- server/src/move/index.js
+++ server/src/move/index.js
@@ -1,51 +1,45 @@
-import express from "express";
 import db from "../db.js";
-import handleError from "../../error.js";
 
-const router = express.Router();
-
-router.post("/", async (req, res) => {
+export default async function handleMove({ gameId, playerId, side, column }) {
   try {
     const game = await db.game.findUnique({
       where: {
-        id: req.body.gameId,
+        id: gameId,
       },
     });
 
-    const piece = req.body.playerId === game.player1Id ? "X" : "O";
+    const piece = playerId === game.player1Id ? "X" : "O";
 
     await db.move.create({
       data: {
-        gameId: req.body.gameId,
-        playerId: req.body.playerId,
-        side: req.body.side,
-        column: req.body.column,
+        gameId,
+        playerId,
+        side,
+        column,
       },
     });
 
     const { board } = game;
 
     // get the column
-    const column = board[req.body.column - 1];
+    const columnData = board[column - 1];
     // get the first empty space, get from right if side is R
     const row =
-      req.body.side === "R" ? column.lastIndexOf("_") : column.indexOf("_");
+      side === "R" ? columnData.lastIndexOf("_") : columnData.indexOf("_");
     // set the space to the player's id
-    board[req.body.column - 1][row] = piece;
+    board[column - 1][row] = piece;
 
     const updatedGame = await db.game.update({
       where: {
-        id: req.body.gameId,
+        id: gameId,
       },
       data: {
         board,
       },
     });
 
-    return res.json(updatedGame);
+    return updatedGame.board;
   } catch (e) {
-    return handleError(e, res);
+    return console.log(e);
   }
-});
-
-export default router;
+}
